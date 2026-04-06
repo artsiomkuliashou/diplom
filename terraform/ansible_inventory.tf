2de5a634985e32166c132e45639623d5bb88a7cd
Ubuntu <ubuntu@ip-172-31-45-47.eu-central-1.compute.internal>
2026-04-06 19:46:21 +0000

    fix app2server

--- terraform/ansible_inventory.tf
+++ terraform/ansible_inventory.tf
@@ -7,3 +7,14 @@ resource "local_file" "ansible_inventory" {
 
   filename = "${path.module}/../ansible/inventory/hosts.ini"
 }
+
+resource "local_file" "ansible_group_vars" {
+  content = templatefile("${path.module}/templates/all.yml.tftpl", {
+    app_private_ip        = aws_instance.app.private_ip
+    app2_private_ip       = aws_instance.app2.private_ip
+    monitoring_private_ip = aws_instance.monitoring.private_ip
+    vpc_cidr              = var.vpc_cidr
+  })
+
+  filename = "${path.module}/../ansible/inventory/group_vars/all.yml"
+}
